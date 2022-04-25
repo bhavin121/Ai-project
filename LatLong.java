a203da8a98a4f5cadb56ca2fba4396f5c81ab21c
Bhavin Prajapat <78008923+bhavin121@users.noreply.github.com>
2022-04-25 21:31:50 +0530

    Create LatLong.java

---
+++ LatLong.java
@@ -0,0 +1,52 @@
+package com.code_n_droid.dwell;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class LatLong {
+
+    @SerializedName("latitude")
+    @Expose
+    private String latitude;
+    @SerializedName("longitude")
+    @Expose
+    private String longitude;
+
+    private int rank;
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString( ){
+        return "LatLong{" +
+                "latitude='" + latitude + '\'' +
+                ", longitude='" + longitude + '\'' +
+                ", rank=" + rank +
+                '}';
+    }
+}
+
